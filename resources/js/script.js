92809df11a5e016b9c47893c843544ba9bb1b348
Aaron Varga <aaronvarga@onmail.com>
2024-01-13 21:02:48 -0500

    coming soon/placeholder page added

---
+++ resources/js/script.js
@@ -0,0 +1,21 @@
+fetch("/resources/data/data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    // console.log(data.image);
+    // console.log(data.name);
+    // console.log(data.username);
+    // console.log(data.bio);
+
+    let userImage = document.querySelector(".user-photo");
+    userImage.src = data.image;
+
+    let myName = document.querySelector(".name");
+    myName.innerHTML = data.name;
+
+    let username = document.querySelector(".username");
+    username.innerHTML = data.username;
+
+    let userBio = document.querySelector(".user-bio");
+    userBio.innerHTML = data.bio;
+  })
+  .catch((error) => console.error(error));
